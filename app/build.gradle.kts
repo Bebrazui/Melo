f201025ce93738501b7cd4b403f9b46d529b0f82
bebrazui <big.reiks@gmail.com>
2026-08-29 10:34:56 +0500

    Bump versionCode to 4 and versionName to 0.3 for RuStore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.melo.music"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.2"
+        versionCode = 4
+        versionName = "0.3"
 
         vectorDrawables {
             useSupportLibrary = true
